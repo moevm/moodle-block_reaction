7c7b03cbe1684c5e97a22502d243cf06a151d5fd
TheShenk <udod_maksim@mail.ru>
2020-09-28 12:04:51 +0300

    dummy plugin done

---
+++ lang/en/block_reaction.php
@@ -0,0 +1,5 @@
+<?php
+$string['pluginname'] = 'Reactions';
+$string['reaction'] = 'Reaction Description';
+$string['reaction:addinstance'] = 'Add a new simple HTML block';
+$string['reaction:myaddinstance'] = 'Add a new simple HTML block to the My Moodle page'; 
