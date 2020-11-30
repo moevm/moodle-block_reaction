e1df6611ab8e4d4f8afa050898205ab6eb876396
TheShenk <udod_maksim@mail.ru>
2020-11-30 12:10:37 +0300

    pdf fix

--- src/export/pdf.php
+++ src/export/pdf.php
@@ -1,5 +1,5 @@
 <?php
-require_once(__DIR__ . '/../../config.php');
+require_once(__DIR__ . '/../../../config.php');
 require_once($CFG->libdir . '/pdflib.php'); 
 require_once($CFG->dirroot . '/course/lib.php'); 
 
@@ -11,6 +11,9 @@ $exportactivities = get_array_of_activities($courseid);
 foreach($exportactivities as $activity) {
     $activity->likes = $DB->count_records('reactions', ['moduleid' => $activity->cm, 'reaction' => true]);
     $activity->dislikes = $DB->count_records('reactions', ['moduleid' => $activity->cm, 'reaction' => false]);
+    $activity->total = $activity->likes + $activity->dislikes;
+    $activity->likes_part = $activity->total ? round($activity->likes / $activity->total * 100, 0) : 0;
+    $activity->dislikes_part = $activity->total ? round($activity->dislikes / $activity->total * 100, 0) : 0;
 }
 
 $pdfwriter = new pdf();
@@ -31,13 +34,18 @@ $html = html_writer::start_tag('table', ['cellspacing' => 0, 'cellpadding' => 2,
     . html_writer::end_tag('tbody');
     
 $total_likes = 0;
+$total_likes_part = 0;
 $total_dislikes = 0;
+$total_dislikes_part = 0;
 foreach($exportactivities as $activity) {
     $activity->likes = $DB->count_records('reactions', ['moduleid' => $activity->cm, 'reaction' => true]);
     $activity->dislikes = $DB->count_records('reactions', ['moduleid' => $activity->cm, 'reaction' => false]);
     
     $total_likes += $activity->likes;
+    $total_likes_part += $activity->likes_part;
+    
     $total_dislikes += $activity->dislikes;
+    $total_dislikes_part += $activity->dislikes_part;
     
     $html .= html_writer::start_tag('tr')
             . html_writer::tag('td', $activity->name)
@@ -68,9 +76,15 @@ $html = html_writer::start_tag('table', ['cellspacing' => 0, 'cellpadding' => 2,
         . html_writer::end_tag('tr')
 
         . html_writer::start_tag('tr')
-            . html_writer::tag('td', '% от всего')
+            . html_writer::tag('td', 'Всего, %')
             . html_writer::tag('td', ($total ? round($total_likes / $total * 100, 0) : 0) . '%') 
             . html_writer::tag('td', ($total ? round($total_dislikes / $total * 100, 0) : 0) . '%') 
+        . html_writer::end_tag('tr')
+        
+        . html_writer::start_tag('tr')
+            . html_writer::tag('td', 'В среднем, %')
+            . html_writer::tag('td', (count($exportactivities) ? round($total_likes_part / count($exportactivities), 0) : 0) . '%') 
+            . html_writer::tag('td', (count($exportactivities) ? round($total_dislikes_part / count($exportactivities), 0) : 0) . '%') 
         . html_writer::end_tag('tr')
         
     . html_writer::end_tag('table');
